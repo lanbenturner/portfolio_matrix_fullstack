9fbaa02984603c97e88beb2b48e882e9bceffa12
lanbenturner <141177007+lanbenturner@users.noreply.github.com>
2024-03-19 12:13:08 -0600

    Re-added userProfile model

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-import logging
+from .models import UserProfile
 
-logger = logging.getLogger(__name__)
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['preferred_name', 'phone_number', 'city', 'state', 'address_line1', 'address_line2']
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(required=False)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile', None)
+        user = User.objects.create_user(**validated_data)
+        if profile_data:
+            UserProfile.objects.create(user=user, **profile_data)
+        return user
