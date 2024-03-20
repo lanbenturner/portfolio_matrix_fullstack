31f83caac051681669d86db8c42f7787dafafa1e
lanbenturner <141177007+lanbenturner@users.noreply.github.com>
2024-03-20 16:49:09 -0600

    re-added frontend starter-kit

---
+++ frontend/src/navigation/vertical/index.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: 'Home',
+    to: { name: 'index' },
+    icon: { icon: 'bx-home-alt' },
+  },
+  {
+    title: 'Second page',
+    to: { name: 'second-page' },
+    icon: { icon: 'bx-file-blank' },
+  },
+]
