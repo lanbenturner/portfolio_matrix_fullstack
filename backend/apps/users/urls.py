afe659017c1387c2c76e2df18686c8a11857794a
lanbenturner <141177007+lanbenturner@users.noreply.github.com>
2024-03-28 14:33:41 -0600

    Fixed double paths in urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -8,7 +8,7 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/register/', RegisterUserAPIView.as_view(), name='register'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterUserAPIView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
