52090bec11f6302d0c87855d273506b8366bbc44
lanbenturner <141177007+lanbenturner@users.noreply.github.com>
2024-03-28 12:08:32 -0600

    Removed Address line 2 from UserProfile model.

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -42,7 +42,6 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=100, blank=True, null=True)
     state = models.CharField(max_length=100, blank=True, null=True)
     address_line1 = models.CharField(max_length=255, blank=True, null=True)
-    address_line2 = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.user.email
