ff0a4a18bdc22342cece272bc3b5938db12d7cb0
lanbenturner <141177007+lanbenturner@users.noreply.github.com>
2024-04-05 16:29:28 -0600

    Reverted to original v1 auth pages

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,41 +1,45 @@
 // ** React Imports
-import { ChangeEvent, ReactNode, useState } from 'react'
+import { useState, ReactNode, MouseEvent } from 'react'
 
-// ** Next Import
+// ** Next Imports
 import Link from 'next/link'
 
 // ** MUI Components
+import Alert from '@mui/material/Alert'
 import Button from '@mui/material/Button'
 import Divider from '@mui/material/Divider'
 import Checkbox from '@mui/material/Checkbox'
 import TextField from '@mui/material/TextField'
 import InputLabel from '@mui/material/InputLabel'
 import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
 import Box, { BoxProps } from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 import FormControl from '@mui/material/FormControl'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import OutlinedInput from '@mui/material/OutlinedInput'
 import { styled, useTheme } from '@mui/material/styles'
+import FormHelperText from '@mui/material/FormHelperText'
 import InputAdornment from '@mui/material/InputAdornment'
 import FormControlLabel from '@mui/material/FormControlLabel'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
+// ** Third Party Imports
+import * as yup from 'yup'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+// ** Hooks
+import { useAuth } from 'src/hooks/useAuth'
+import useBgColor, { UseBgColorType } from 'src/@core/hooks/useBgColor'
+import { useSettings } from 'src/@core/hooks/useSettings'
+
 // ** Configs
 import themeConfig from 'src/configs/themeConfig'
 
 // ** Layout Import
 import BlankLayout from 'src/@core/layouts/BlankLayout'
-
-// ** Hooks
-import { useSettings } from 'src/@core/hooks/useSettings'
-
-interface State {
-  password: string
-  showPassword: boolean
-}
 
 // ** Styled Components
 const LoginIllustration = styled('img')({
@@ -67,26 +71,54 @@ const LinkStyled = styled(Link)(({ theme }) => ({
   color: theme.palette.primary.main
 }))
 
-const Login = () => {
-  // ** States
-  const [values, setValues] = useState<State>({
-    password: '',
-    showPassword: false
-  })
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(5).required()
+})
 
-  // ** Hook
+const defaultValues = {
+  password: 'admin',
+  email: 'admin@sneat.com'
+}
+
+interface FormData {
+  email: string
+  password: string
+}
+
+const LoginPage = () => {
+  const [rememberMe, setRememberMe] = useState<boolean>(true)
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+
+  // ** Hooks
+  const auth = useAuth()
   const theme = useTheme()
   const { settings } = useSettings()
+  const bgColors: UseBgColorType = useBgColor()
   const hidden = useMediaQuery(theme.breakpoints.down('lg'))
 
   // ** Var
   const { skin } = settings
 
-  const handleChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword })
+  const {
+    control,
+    setError,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues,
+    mode: 'onBlur',
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = (data: FormData) => {
+    const { email, password } = data
+    auth.login({ email, password, rememberMe }, () => {
+      setError('email', {
+        type: 'manual',
+        message: 'Email or Password is invalid'
+      })
+    })
   }
 
   return (
@@ -141,39 +173,80 @@ const Login = () => {
           <Typography sx={{ mb: 6, color: 'text.secondary' }}>
             Please sign-in to your account and start the adventure
           </Typography>
-          <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
-            <TextField autoFocus fullWidth id='email' label='Email' sx={{ mb: 4 }} />
-            <FormControl fullWidth sx={{ mb: 2 }}>
-              <InputLabel htmlFor='auth-login-v2-password'>Password</InputLabel>
-              <OutlinedInput
-                label='Password'
-                value={values.password}
-                id='auth-login-v2-password'
-                onChange={handleChange('password')}
-                type={values.showPassword ? 'text' : 'password'}
-                endAdornment={
-                  <InputAdornment position='end'>
-                    <IconButton
-                      edge='end'
-                      onClick={handleClickShowPassword}
-                      onMouseDown={e => e.preventDefault()}
-                      aria-label='toggle password visibility'
-                    >
-                      <Icon fontSize={20} icon={values.showPassword ? 'bx:show' : 'bx:hide'} />
-                    </IconButton>
-                  </InputAdornment>
-                }
+          <Alert icon={false} sx={{ py: 3, mb: 6, ...bgColors.primaryLight, '& .MuiAlert-message': { p: 0 } }}>
+            <Typography variant='caption' sx={{ mb: 2, display: 'block', color: 'primary.main' }}>
+              Admin: <strong>admin@sneat.com</strong> / Pass: <strong>admin</strong>
+            </Typography>
+            <Typography variant='caption' sx={{ display: 'block', color: 'primary.main' }}>
+              Client: <strong>client@sneat.com</strong> / Pass: <strong>client</strong>
+            </Typography>
+          </Alert>
+          <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <Controller
+                name='email'
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <TextField
+                    autoFocus
+                    label='Email'
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={onChange}
+                    error={Boolean(errors.email)}
+                    placeholder='admin@sneat.com'
+                  />
+                )}
               />
+              {errors.email && <FormHelperText sx={{ color: 'error.main' }}>{errors.email.message}</FormHelperText>}
+            </FormControl>
+            <FormControl fullWidth sx={{ mb: 2 }}>
+              <InputLabel htmlFor='auth-login-v2-password' error={Boolean(errors.password)}>
+                Password
+              </InputLabel>
+              <Controller
+                name='password'
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <OutlinedInput
+                    value={value}
+                    onBlur={onBlur}
+                    label='Password'
+                    onChange={onChange}
+                    id='auth-login-v2-password'
+                    error={Boolean(errors.password)}
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                      <InputAdornment position='end'>
+                        <IconButton
+                          edge='end'
+                          onMouseDown={e => e.preventDefault()}
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          <Icon fontSize={20} icon={showPassword ? 'bx:show' : 'bx:hide'} />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                )}
+              />
+              {errors.password && (
+                <FormHelperText sx={{ color: 'error.main' }} id=''>
+                  {errors.password.message}
+                </FormHelperText>
+              )}
             </FormControl>
             <Box
               sx={{ mb: 4, display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-between' }}
             >
               <FormControlLabel
                 label='Remember Me'
-                control={<Checkbox />}
                 sx={{ '& .MuiFormControlLabel-label': { fontSize: '0.875rem', color: 'text.secondary' } }}
+                control={<Checkbox checked={rememberMe} onChange={e => setRememberMe(e.target.checked)} />}
               />
-              <LinkStyled href='/pages/auth/forgot-password-v2'>Forgot Password?</LinkStyled>
+              <LinkStyled href='/forgot-password'>Forgot Password?</LinkStyled>
             </Box>
             <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 4 }}>
               Sign in
@@ -183,26 +256,41 @@ const Login = () => {
                 New on our platform?
               </Typography>
               <Typography>
-                <LinkStyled href='/pages/auth/register-v2'>Create an account</LinkStyled>
+                <LinkStyled href='/register'>Create an account</LinkStyled>
               </Typography>
             </Box>
             <Divider sx={{ my: `${theme.spacing(6)} !important` }}>or</Divider>
             <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <IconButton href='/' component={Link} sx={{ color: '#497ce2' }} onClick={e => e.preventDefault()}>
+              <IconButton
+                href='/'
+                component={Link}
+                sx={{ color: '#497ce2' }}
+                onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+              >
                 <Icon icon='bxl:facebook-circle' />
               </IconButton>
-              <IconButton href='/' component={Link} sx={{ color: '#1da1f2' }} onClick={e => e.preventDefault()}>
+              <IconButton
+                href='/'
+                component={Link}
+                sx={{ color: '#1da1f2' }}
+                onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+              >
                 <Icon icon='bxl:twitter' />
               </IconButton>
               <IconButton
                 href='/'
                 component={Link}
-                onClick={e => e.preventDefault()}
+                onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
                 sx={{ color: theme.palette.mode === 'light' ? '#272727' : 'grey.300' }}
               >
                 <Icon icon='bxl:github' />
               </IconButton>
-              <IconButton href='/' component={Link} sx={{ color: '#db4437' }} onClick={e => e.preventDefault()}>
+              <IconButton
+                href='/'
+                component={Link}
+                sx={{ color: '#db4437' }}
+                onClick={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+              >
                 <Icon icon='bxl:google' />
               </IconButton>
             </Box>
@@ -213,6 +301,8 @@ const Login = () => {
   )
 }
 
-Login.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>
+LoginPage.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>
 
-export default Login
+LoginPage.guestGuard = true
+
+export default LoginPage
