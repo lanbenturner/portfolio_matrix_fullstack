d8ad1e96c332c322b605f7ca70439be7afe5826d
lanbenturner <141177007+lanbenturner@users.noreply.github.com>
2024-04-15 10:42:22 -0500

    Turned off Auth Guard

--- frontend/src/navigation/vertical/index.ts
+++ frontend/src/navigation/vertical/index.ts
@@ -6,16 +6,19 @@ const navigation = (): VerticalNavItemsType => {
     {
       title: 'Home',
       path: '/home',
+      auth: false,
       icon: 'bx:home-circle',
     },
     {
       title: 'Second Page',
       path: '/second-page',
+      auth: false,
       icon: 'bx:envelope',
     },
     {
       path: '/acl',
       title: 'Access Control',
+      auth: false,
       icon: 'bx:shield',
     }
   ]
